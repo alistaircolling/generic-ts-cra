5ebca583a41b8a65eef69383817ce993f52ce399
Alistair <a@colli.ng>
2023-09-25 15:20:03 +0100

    feat: types and theme-ui setup

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    primary: 'blue',
+    secondary: 'green',
+    text: 'black',
+  },
+  fonts: {
+    body: 'sans-serif',
+    heading: 'serif',
+  },
+  sizes: {
+    small: 10,
+    medium: 20,
+    large: 30,
+  },
+};
+
+export default theme;
---
+++ src/types/site.d.ts
@@ -0,0 +1,48 @@
+export interface Site {
+  title: string;
+}
+export interface MenuItem {
+  title: string;
+  path: string;
+}
+
+export interface Menu {
+  items: MenuItem[];
+}
+
+export interface Image {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+
+export interface Card {
+  title: string;
+  subtitle: string;
+  image: Image;
+  id: string;
+}
+
+export interface Page {
+  title: string;
+  path: string;
+  cards: Card[];
+}
+
+type Theme = {
+  colors: {
+    primary: string;
+    secondary: string;
+    text: string;
+  };
+  fonts: {
+    body: string;
+    heading: string;
+  };
+  sizes: {
+    small: number;
+    medium: number;
+    large: number;
+  };
+};
