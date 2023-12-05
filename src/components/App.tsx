d963fe8cdb8df87a9aca6f4a0242fc150e37022c
Alistair <a@colli.ng>
2023-12-05 14:31:19 +0000

    feat: custom hook to load data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../assets/logo.svg'
 import { useGetData } from '../hooks/useGetData'
 import theme from '../styles/theme'
-import { Site } from '../types/site'
+import { DataResponse, Site } from '../types/site'
 
 const App: React.FC = () => {
   console.log('app---------------')
 
-  const [data, setData] = useState<Site | undefined>(undefined)
+  const { data, error, loading } = useGetData('site')
 
-  //TODO prevent running on each render
-  useGetData('site').then((res) => setData(res as Site))
+  if (error) return <div>Error</div>
 
   return (
     <div className='App'>
       <div style={{ height: '100vh', width: '100vw', backgroundColor: theme.colors.primary }}>
         {data ? (
           <header>
-            <h1 style={{ margin: 0, color: 'greenyellow', textAlign: 'center' }}>{data?.title}</h1>
+            <h1 style={{ margin: 0, color: 'greenyellow', textAlign: 'center' }}>{(data as Site)?.title}</h1>
             <div>
               <img src={logo} className='App-logo' alt='logo' />
             </div>
--- src/hooks/useGetData.ts
+++ src/hooks/useGetData.ts
@@ -2,18 +2,42 @@ import axios from "axios";
 import { DataRequest, DataResponse } from "../types/site";
 import site from '../data/site.json'
 import menu from '../data/menu.json'
+import { useEffect, useState } from "react";
 
 
-const useGetData = async (dataRequest: DataRequest): Promise<DataResponse | undefined> => {
-  console.log('useGetData', dataRequest)
+const useGetData = (request: DataRequest) => {
+  const [data, setData] = useState<DataResponse | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<Error | null>(null)
 
-  switch (dataRequest) {
-    case 'site':
-      return site
-    case 'menu':
-      return undefined
-  }
+  useEffect(() => {
+    const fetchData = async (request: any) => {
+
+      switch (request) {
+        case 'site':
+          return site
+        case 'menu':
+          return menu
+        default:
+          return null
+      }
+    }
+    fetchData(request);
+  }, [request])
+
+  return { data, loading, error }
 }
+
+// const useGetData = async (dataRequest: DataRequest): Promise<DataResponse | undefined> => {
+//   console.log('useGetData', dataRequest)
+
+//   switch (dataRequest) {
+//     case 'site':
+//       return site
+//     case 'menu':
+//       return menu
+//   }
+// }
 
 export { useGetData }
 
